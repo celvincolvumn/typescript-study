12f8fd13cd72cb162eca695ccb1756ac9fd2f18a
Kevin Song <celvincolvumn@gmail.com>
2020-03-02 13:44:12 +0900

    study type assertions

---
+++ basic-types/type-assertions.ts
@@ -0,0 +1,7 @@
+let someValue1: any = "this is a string";
+
+let strLength1: number = (<string>someValue1).length;
+
+let someValue2: any = "this is a string";
+
+let strLength2: number = (someValue2 as string).length;
