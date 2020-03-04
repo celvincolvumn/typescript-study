1f19eeb23c8e8d53b18d6b7137fe20349513801d
Kevin Song <celvincolvumn@gmail.com>
2020-03-04 19:02:43 +0900

    study variable declarations

---
+++ variable-declarations/re-declarations-and-shadowing.ts
@@ -0,0 +1,46 @@
+// function f(x) {
+//     var x;
+//     var x;
+//
+//     if (true) {
+//         var x;
+//     }
+// }
+
+
+// let x = 10;
+// let x = 20;
+
+// function f(x) {
+//   let x = 100; // error: duplicate identifier "x"
+// }
+//
+// function g() {
+//   let x = 100;
+//   var x = 100; // error: cannot
+// }
+
+// function f(condition, x) {
+//   if (condition) {
+//     let x = 100;
+//     return x;
+//   }
+//
+//   return x;
+// }
+//
+// f(false, 0); // '0' return
+// f(true, 0);  // '100' return
+
+
+// function sumMatrix(matrix: number[][]) {
+//   let sum = 0;
+//   for (let i = 0; i < matrix.length; i++) {
+//     var currentRow = matrix[i];
+//     for (let i = 0; i < currentRow.length; i++) {
+//       sum += currentRow[i];
+//     }
+//   }
+//
+//   return sum;
+// }
